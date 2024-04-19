a963847ca58c5a9869e0d5550a8a7b9d5c24f654
Alexander Leinich <alexanderleinich1@gmail.com>
2024-04-19 18:57:13 +0200

    Refactor code to improve readability

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,13 @@ fn lowest(
 }
 
 fn lowess(params: &Loess) -> Vec<f64> {
-    let Loess { x, y, f, nsteps, delta } = params;
+    let Loess {
+        x,
+        y,
+        f,
+        nsteps,
+        delta,
+    } = params;
     let n = x.len();
     if n < 2 {
         return y.to_vec();
@@ -56,9 +62,8 @@ fn lowess(params: &Loess) -> Vec<f64> {
     let mut res = vec![0.0; n];
 
     let mut last = 0;
-    let mut iter = 1;
 
-    while iter <= nsteps + 1 {
+    for iter in 1..=nsteps + 1 {
         let mut nleft = 1;
         let mut nright = ns;
         let mut i = 1;
@@ -77,7 +82,7 @@ fn lowess(params: &Loess) -> Vec<f64> {
             }
 
             // Compute the local weighted regression
-            if let Some(ys) = lowest(
+            match lowest(
                 x,
                 &y,
                 x[i - 1],
@@ -87,19 +92,20 @@ fn lowess(params: &Loess) -> Vec<f64> {
                 iter > 1,
                 &rw,
             ) {
-                if last < i - 1 {
-                    // Perform linear interpolation within the segment
-                    interpolate_segment(x, &mut y, &res, last - 1, i - 1);
-                }
+                Some(ys) => {
+                    if last < i - 1 {
+                        interpolate_segment(x, &mut y, &res, last - 1, i - 1);
+                    }
 
-                y[i - 1] = ys;
-                last = i;
+                    y[i - 1] = ys;
+                    last = i;
+                }
+                None => break,
             }
 
             let cut = x[last - 1] + delta;
-            i = last;
 
-            while i <= n {
+            for i in 1..=n {
                 if x[i - 1] > cut {
                     break;
                 }
@@ -107,7 +113,6 @@ fn lowess(params: &Loess) -> Vec<f64> {
                     y[i - 1] = y[last - 1];
                     last = i;
                 }
-                i += 1;
             }
 
             i = last + 1;
@@ -136,8 +141,7 @@ fn lowess(params: &Loess) -> Vec<f64> {
 
         update_residual_weights_with_cmad(&mut rw, &res, cmad);
 
-        iter += 1;
-    };
+    }
 
     y
 }
@@ -166,7 +170,7 @@ fn update_residual_weights(rw: &mut Vec<f64>, res: &[f64], n: usize) {
     for i in 0..n {
         rw[i] = res[i].abs();
     }
-    rw.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    rw.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap_or(std::cmp::Ordering::Equal));
 }
 
 fn compute_cmad(rw: &[f64], n: usize) -> f64 {
@@ -206,7 +210,7 @@ impl Loess {
             y: y.to_vec(),
             f,
             nsteps,
-            delta
+            delta,
         };
         lowess(&params)
     }
@@ -218,4 +222,26 @@ pub struct Loess {
     pub f: f64,
     pub nsteps: usize,
     pub delta: f64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lowess() {
+
+
+        let y = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0, 30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0];
+        let x: Vec<f64> = (1..(y.len() + 1)).map(|x| x as f64).collect();
+
+        let y_assert = [5.5037337686781305, 6.114970675649464, 6.658262751576383, 7.1403695282317345, 7.577255689830473, 8.003882873231403, 8.487312705799924, 9.104067724255362, 9.883408822063812, 10.692715467291649, 11.536654173833748, 12.41420852412808, 13.320802917860432, 14.249910217842503, 15.195352387869017, 16.152895316931946, 17.120064517689144, 18.0948463179265, 19.075531777772667, 20.060753724127814, 21.049471607765014, 22.040899678791135, 23.03443254949329, 24.02959665501051, 25.026023028668774, 26.023426261087803, 27.02158600478072, 28.020331793653238, 29.019531502277275, 30.019082719566082, 31.01890613178673, 32.018940337133635, 33.01913779273324, 34.019461677573574, 35.01988347315761, 36.02038109324451, 37.02093743410303, 38.02153925039922, 39.0221762844904, 40.022840592418355, 41.02352602208565, 42.024227809117434, 43.024942263837204, 43.890130077204894, 44.438386008569374, 44.72861697990426, 44.83840555704233, 44.799400990727925, 44.61138455825291, 44.25380138245755];
+
+        let f = 0.33333;
+        let nsteps = 0;
+        let delta = 0.0;
+        let loess = Loess::new(&x, &y, f, nsteps, delta);
+        println!("{:?}", loess);
+        assert_eq!(loess, y_assert);
+    }
 }
